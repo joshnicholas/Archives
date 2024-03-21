cdb59721e4d11714d77dead514894dfcd2561b3a
Josh Nicholas <hi@thambi.li>
2024-03-22 08:30:00 +1100

    New scrapers

--- Scrapers/top_stories.py
+++ Scrapers/top_stories.py
@@ -41,7 +41,7 @@ def create_search(what, frame):
     return copier
 
 
-def shot_grabber(urlo, publication, javascript_code):
+def shot_grabber(urlo, publication, out_path, javascript_code):
 
     result = subprocess.check_output(['shot-scraper', 'javascript', urlo, javascript_code, "--user-agent", "Firefox"])
 
@@ -58,21 +58,61 @@ def shot_grabber(urlo, publication, javascript_code):
     frame['scraped_datetime']= format_scrape_time 
 
     frame = frame[['publication', 'scraped_datetime', 'Headline', 'Url', 'Rank', 'Search_var']]
-    print(frame)
-    print(frame.columns.tolist())
+    # print(frame)
+    # print(frame.columns.tolist())
+
+    with open(f'{out_path}/latest.json', 'w') as f:
+        frame.to_json(f, orient='records')
+
+    with open(f'{out_path}/daily_dumps/{format_scrape_time}.json', 'w') as f:
+        frame.to_json(f, orient='records')
+
     return frame 
 
 
-smh = shot_grabber('https://www.smh.com.au/', 'SMH',
-    """
-    var contexto = document.querySelector('[data-an-name="Most Popular"]')
-    Array.from(contexto.querySelectorAll('[data-testid="article-headline"]'), el => {
-    let Headline = el.querySelector('a').innerText;
-    let Url = el.querySelector('a')['href']
+print("Scraping the SMH")
+
+# smh = shot_grabber('https://www.smh.com.au/', 'SMH', 'Archive/smh_top',
+#     """
+#     var contexto = document.querySelector('[data-an-name="Most Popular"]')
+#     Array.from(contexto.querySelectorAll('[data-testid="article-headline"]'), el => {
+#     let Headline = el.querySelector('a').innerText;
+#     let Url = el.querySelector('a')['href']
+#     return {Headline, Url};
+#     })""")
+
+
+print("Scraping the ABC")
+
+# abc = shot_grabber('https://www.abc.net.au/news', 'ABC', 'Archive/abc_top',
+#  """
+# Array.from(document.querySelectorAll('[data-uri="recommendation://collection/abc-news-homepage-sidebar"]'), el => {
+#     let Headline = el.querySelector('a').innerText;
+#     let Url = el.querySelector('a')['href']
+#     return {Headline, Url};
+
+# })""")
+
+# print("Scraping News")
+
+# news = shot_grabber('https://www.news.com.au/', 'News', 'Archive/newscom_top',
+#     javascript_code = """
+#     var contexto = document.querySelector('.most-popular-content')
+#     Array.from(contexto.querySelectorAll('a'), el => {
+#     let Headline = el.innerText;
+#     let Url = el['href']
+#     return {Headline, Url};
+#     })""")
+
+
+print("Scraping Graun")
+
+graun = shot_grabber('https://www.theguardian.com/au', 'The Guardian', 'Archive/graun_top',
+    javascript_code = """
+    var contexto = document.querySelector('[data-link-name="most-viewed"]')
+    Array.from(contexto.querySelectorAll('a'), el => {
+    let Headline = el.innerText;
+    let Url = el['href']
     return {Headline, Url};
     })""")
-
-with open(f'Archive/smh_top/latest_test_two.json', 'w') as f:
-    smh.to_json(f, orient='records')
-
 
